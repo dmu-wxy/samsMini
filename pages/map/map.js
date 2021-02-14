477cc710f6064e244b7ef975e22f8a62d4765b3b
clever_WangErgou <48337433+dmu-wxy@users.noreply.github.com>
2021-02-14 17:36:11 +0800

    feat 地图、我的 页面

--- pages/map/map.js
+++ pages/map/map.js
@@ -5,14 +5,67 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    ScreenTotalW: 750,
+    ScreenTotalH: -1,
+    latitude: 23.099994,
+    longitude: 113.324520,
+    markers: [{
+      id: 1,
+      latitude: 23.099994,
+      longitude: 113.324520,
+      name: 'T.I.T 创意园'
+    }],
+    covers: [{
+      latitude: 23.099994,
+      longitude: 113.344520,
+      iconPath: '/image/map/location.png'
+    }, {
+      latitude: 23.099994,
+      longitude: 113.304520,
+      iconPath: '/image/map/location.png'
+    }]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      ScreenTotalH: 750 * wx.getSystemInfoSync().screenHeight /wx.getSystemInfoSync().screenWidth
+    })
+    let that = this;
+    wx.getSetting({
+      success(res) {
+        if (!res.authSetting['scope.userLocation']) {
+          wx.authorize({
+            scope: 'scope.userLocation',
+            success () {
+              wx.getLocation({
+                type: 'gcj02', //返回可以用于wx.openLocation的经纬度
+                success (res) {
+                  that.setData({
+                    longitude:res.longitude,
+                    latitude:res.latitude
+                  });
+                  console.log(res.longitude + "," + res.latitude);
+                }
+               })
+            }
+          })
+        }else{
+          wx.getLocation({
+            type: 'gcj02', //返回可以用于wx.openLocation的经纬度
+            success (res) {
+              that.setData({
+                longitude:res.longitude,
+                latitude:res.latitude
+              });
+              console.log(res.longitude + "," + res.latitude);
+            }
+           })
+        }
+      }
+    })
   },
 
   /**
